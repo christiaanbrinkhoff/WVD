e5c68e18b953eb75809ee0e8aa2b47e873de204f
Christiaan Brinkhoff <55492283+christiaanbrinkhoff@users.noreply.github.com>
2020-06-09 21:48:31 +0200

    JSON upload

--- scripts/customapps.ps1
+++ scripts/customapps.ps1
@@ -1,4 +1,4 @@
-# variables
+# define variables
 $fsLogixURL="https://aka.ms/fslogix_download"
 $installerFile="fslogix_download.zip"
 $path="c:\temp"
@@ -12,14 +12,23 @@ Expand-Archive $path\$installerFile -DestinationPath $path\fsLogix\extract
 Start-Process -FilePath $path\fsLogix\extract\x64\Release\FSLogixAppsSetup.exe -Args "/install /quiet /norestart" -Wait
 Write-Host "Fslogix Install Succeeded"
 
-#Install VSCode
+#Configure FSLogix
+New-Item -Path "HKLM:\Software\FSLogix\"
+New-Item -Path "HKLM:\Software\FSLogix\Profiles"
+Set-ItemProperty -Path "HKLM:\Software\FSLogix\Profiles" -Name "Enabled" -Value 1
+Set-ItemProperty -Path "HKLM:\Software\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1
+Set-ItemProperty -Path "HKLM:\Software\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1
+Set-ItemProperty -Path "HKLM:\Software\FSLogix\Profiles" -Name "VolumeType" -Value VHDx
+Set-ItemProperty -Path "HKLM:\Software\FSLogix\Profiles" -Name "VHDLocations" -Value "fslogixwvddemo.file.core.windows.net\fslogixprofiles"
+
+#Install Teams per-machine
 Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile '$path\VScode.exe'
 Invoke-Expression -Command '$path\VScode.exe /verysilent'
 
 #Start sleep
 Start-Sleep -Seconds 10
 
-#InstallNotepadplusplus
+#Install Notepad++
 Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile '$path\notepadplusplus.exe'
 Invoke-Expression -Command '$path\notepadplusplus.exe /S'
 
